4c305eef29ef6e79635c284e503e55df56478a13
Matt Sell <mwsell@gmail.com>
2015-08-12 22:15:14 -0500

    change rakings icon

--- src/client/app/rankings/rankings.route.js
+++ src/client/app/rankings/rankings.route.js
@@ -23,7 +23,7 @@
                     title: 'rankings',
                     settings: {
                         nav: 2,
-                        content: '<i class="fa fa-dashboard"></i> Rankings'
+                        content: '<i class="fa fa-sort-numeric-asc"></i> Rankings'
                     }
                 }
             }
