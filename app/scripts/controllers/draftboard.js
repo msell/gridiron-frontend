da8d6de3a1aa9f0442454032978004594225ca8a
Matt Sell <mwsell@gmail.com>
2015-08-24 20:51:13 -0500

    ng repeat problem

--- app/scripts/controllers/draftboard.js
+++ app/scripts/controllers/draftboard.js
@@ -1,62 +1,33 @@
 (function () {
-    'use strict';
+  'use strict';
 
-    angular.module('gridironFrontend')
-        .controller('WeighinCtrl', function ($scope, $http, configuration, alert, $auth, $state) {
+  angular.module('gridironFrontend')
+    .controller('DraftboardCtrl', function ($scope, $http, configuration, alert, $auth, $state) {
 
-            if (!$auth.isAuthenticated()) {
-                $state.go('login');
-            }
+      if (!$auth.isAuthenticated()) {
+        $state.go('login');
+      }
 
-            $scope.addEntry = function () {
-                $scope.weighIns.push({
-                    'weight': $scope.weight,
-                    'date': $scope.date
-                });
+      //            $scope.addEntry = function () {
+      //                $scope.weighIns.push({
+      //                    'weight': $scope.weight,
+      //                    'date': $scope.date
+      //                });
+      //
+      //            };
 
-            };
+      $scope.pageSize = 50;
+      $scope.pageNumber = 1;     
+      $scope.players = [];
 
-            $scope.getGoal = function () {
-                $http.get(configuration.apiUrl + 'weightlossgoal')
-            };
 
-            $scope.goals = [];
 
-            $scope.weight = 0; // todo: default to last weigh in, add up/down control
-            $scope.date = new Date();
-
-            $scope.weighIns = [
-                {
-                    'weight': 250,
-                    'date': '2015-01-02T04:45:14.370Z'
-            },
-                {
-                    'weight': 242,
-                    'date': '2015-01-02T04:45:14.370Z'
-            },
-                {
-                    'weight': 240,
-                    'date': '2015-01-02T04:45:14.370Z'
-            },
-                {
-                    'weight': 233,
-                    'date': '2015-01-02T04:45:14.370Z'
-            },
-                {
-                    'weight': 237,
-                    'date': '2015-01-02T04:45:14.370Z'
-            },
-                {
-                    'weight': 233,
-                    'date': '2015-01-02T04:45:14.370Z'
-            }
-                      ];
-
-            //        $http.get(configuration.apiUrl + 'weighIn').success(function (weighIns) {
-            //            $scope.weighIns = weighIns;
-            //        }).error(function (err) {
-            //            alert('warning', 'unable to get weighins', err.message);
-            //        })
-        });
+      $http.get(configuration.apiUrl + 'player?limit=' + $scope.pageSize + '&skip=' + $scope.pageNumber * $scope.pageSize - $scope.pageSize)
+        .success(function (data) {
+        $scope.players = data;
+      }).error(function (err) {
+        alert('warning', 'unable to get player data', err.message);
+      })
+    });
 
 })();
